4913603150f9c813f9146d244adf7b049aa6552d
LewisDwy2000 <119431339+LewisDwy2000@users.noreply.github.com>
2022-12-06 14:18:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 #Fruitvice API response
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon').json()
+streamlit.text(fruityvice_response)
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response)
+# Display as a table
+streamlit.dataframe(fruityvice_normalized)
