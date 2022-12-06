b496576568b39e6be3bb100c797288ea2fca1482
LewisDwy2000 <119431339+LewisDwy2000@users.noreply.github.com>
2022-12-06 16:55:14 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
-
 streamlit.header("View Our Fruit List - Add Your Favourites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
